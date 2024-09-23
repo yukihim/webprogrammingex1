d66c72c520319b6d6edad571fe5b228675f96145
Yuyu <jacklorien@gmail.com>
2024-09-23 22:22:37 +0700

    some error with Ward

--- scripts.js
+++ scripts.js
@@ -1,29 +1,446 @@
-// scripts.js
-const citiesByProvince = {
-    Hanoi: ["Ba Đình", "Hoàn Kiếm", "Tây Hồ", "Long Biên", "Cầu Giấy"],
-    HCMC: ["Quận 1", "Quận 2", "Quận 3", "Quận 7", "Thủ Đức"],
-    Danang: ["Hải Châu", "Sơn Trà", "Ngũ Hành Sơn", "Cẩm Lệ", "Liên Chiểu"],
-    CanTho: ["Ninh Kiều", "Bình Thủy", "Cái Răng", "Ô Môn", "Thốt Nốt"],
-    Hue: ["Phú Nhuận", "Hương Thủy", "Hương Trà", "Phong Điền", "Quảng Điền"]
-  };
-  
-  function updateCities() {
-    const provinceSelect = document.getElementById("province");
-    const citySelect = document.getElementById("city");
-  
-    // Clear the existing cities/districts
-    citySelect.innerHTML = '<option value="">Select a city/district</option>';
-  
-    const selectedProvince = provinceSelect.value;
-  
-    if (selectedProvince && citiesByProvince[selectedProvince]) {
-      // Populate cities/districts based on the selected province
-      citiesByProvince[selectedProvince].forEach(function(city) {
-        const option = document.createElement("option");
-        option.value = city;
-        option.textContent = city;
-        citySelect.appendChild(option);
-      });
+var countryProvinceCityDistrictWard = {
+  "Viet Nam": {
+    "Ha Noi": {
+      "Ba Dinh": {},
+      "Hoan Kiem": {},
+      "Tay Ho": {},
+      "Hai Ba Trung": {},
+      "Dong Da": {},
+      "Cau Giay": {},
+    },
+    "Ho Chi Minh": {
+      "D1": {
+        "Ben Thanh": {}, 
+        "Ben Nghe": {}, 
+        "Da Kao": {}, 
+        "Nguyen Cu Trinh": {}, 
+        "Nguyen Thai Binh": {}, 
+        "Tan Dinh": {}, 
+        "Co Giang": {}, 
+        "Cau Ong Lanh": {}, 
+        "Pham Ngu Lao": {}, 
+        "Cau Kho": {}
+      },
+      "D3": {
+        "Ward 1": {}, 
+        "Ward 2": {}, 
+        "Ward 3": {}, 
+        "Ward 4": {}, 
+        "Ward 5": {}, 
+        "Ward 9": {}, 
+        "Ward 10": {}, 
+        "Ward 11": {}, 
+        "Ward 12": {}, 
+        "Ward 13": {}, 
+        "Ward 14": {}, 
+        "Vo Thi Sau": {}
+      },
+      "D4": {
+        "Ward 1": {}, 
+        "Ward 2": {}, 
+        "Ward 3": {}, 
+        "Ward 4": {}, 
+        "Ward 6": {}, 
+        "Ward 8": {}, 
+        "Ward 9": {}, 
+        "Ward 10": {}, 
+        "Ward 13": {}, 
+        "Ward 14": {}, 
+        "Ward 15": {}, 
+        "Ward 16": {}, 
+        "Ward 18": {}
+      },
+      "D5": {
+        "Ward 1": {}, 
+        "Ward 2": {}, 
+        "Ward 3": {}, 
+        "Ward 4": {}, 
+        "Ward 5": {}, 
+        "Ward 6": {}, 
+        "Ward 7": {}, 
+        "Ward 8": {}, 
+        "Ward 9": {}, 
+        "Ward 10": {}, 
+        "Ward 11": {}, 
+        "Ward 12": {}, 
+        "Ward 13": {}, 
+        "Ward 14": {}
+      },
+      "D6": {
+        "Ward 1": {}, 
+        "Ward 2": {}, 
+        "Ward 3": {}, 
+        "Ward 4": {}, 
+        "Ward 5": {}, 
+        "Ward 6": {}, 
+        "Ward 7": {}, 
+        "Ward 8": {}, 
+        "Ward 9": {}, 
+        "Ward 10": {}, 
+        "Ward 11": {}, 
+        "Ward 12": {}, 
+        "Ward 13": {}, 
+        "Ward 14": {}
+      },
+      "D7": {
+        "Ward Phu My": {}, 
+        "Ward Phu Thuan": {}, 
+        "Ward Binh Thuan": {}, 
+        "Ward Tan Phong": {}, 
+        "Ward Tan Phu": {}, 
+        "Ward Tan Hung": {}, 
+        "Ward Tan Kieng": {}, 
+        "Ward Tan Quy": {}, 
+        "Ward Tan Thuan Dong": {}, 
+        "Ward Tan Thuan Tay": {}
+      },
+      "D8": {
+        "Ward 1": {}, 
+        "Ward 2": {}, 
+        "Ward 3": {}, 
+        "Ward 4": {}, 
+        "Ward 5": {}, 
+        "Ward 6": {}, 
+        "Ward 7": {}, 
+        "Ward 8": {}, 
+        "Ward 9": {}, 
+        "Ward 10": {}, 
+        "Ward 11": {}, 
+        "Ward 12": {}, 
+        "Ward 13": {}, 
+        "Ward 14": {}, 
+        "Ward 15": {}, 
+        "Ward 16": {}
+      },
+      "D10": {
+        "Ward 1": {}, 
+        "Ward 2": {}, 
+        "Ward 4": {}, 
+        "Ward 5": {}, 
+        "Ward 6": {}, 
+        "Ward 7": {}, 
+        "Ward 8": {}, 
+        "Ward 9": {}, 
+        "Ward 10": {}, 
+        "Ward 11": {}, 
+        "Ward 12": {}, 
+        "Ward 13": {}, 
+        "Ward 14": {}, 
+        "Ward 15": {}
+      },
+      "D11": {
+        "Ward 1": {}, 
+        "Ward 2": {}, 
+        "Ward 3": {}, 
+        "Ward 4": {}, 
+        "Ward 5": {}, 
+        "Ward 6": {}, 
+        "Ward 7": {}, 
+        "Ward 8": {}, 
+        "Ward 9": {}, 
+        "Ward 10": {}, 
+        "Ward 11": {}, 
+        "Ward 12": {}, 
+        "Ward 13": {}, 
+        "Ward 14": {}, 
+        "Ward 15": {}, 
+        "Ward 16": {}
+      },
+      "D12": {
+        "Ward An Phu Dong": {}, 
+        "Ward Dong Hung Thuan": {}, 
+        "Ward Tan Hung Thuan": {}, 
+        "Ward Hiep Thanh": {}, 
+        "Ward Thoi An": {}, 
+        "Ward Tan Thoi Nhat": {}, 
+        "Ward Tan Thoi Hiep": {}, 
+        "Ward Tan Chanh Hiep": {}, 
+        "Ward Thanh Loc": {}, 
+        "Ward Thanh Xuan": {}, 
+        "Ward Trung My Tay": {}
+      },
+      "Binh Tan": {
+        "Ward An Lac": {}, 
+        "Ward An Lac A": {}, 
+        "Ward Binh Hung Hoa": {}, 
+        "Ward Binh Hung Hoa A": {}, 
+        "Ward Binh Hung Hoa B": {}, 
+        "Ward Tan Tao": {}, 
+        "Ward Tan Tao A": {}, 
+        "Ward Binh Tri Dong A": {}, 
+        "Ward Binh Tri Dong B": {}
+      },
+      "Thu Duc city": {
+        "Ward An Khanh": {}, 
+        "Ward An Phu": {}, 
+        "Ward An Loi Dong": {}, 
+        "Ward Binh Chieu": {}, 
+        "Ward Binh Tho": {}, 
+        "Ward Binh Trung Dong": {}, 
+        "Ward Binh Trung Tay": {}, 
+        "Ward Hiep Phu": {}, 
+        "Ward Hiep Binh Chanh": {}, 
+        "Ward Hiep Binh Phuoc": {}, 
+        "Ward Cat Lai": {}, 
+        "Ward Linh Trung": {}, 
+        "Ward Linh Xuan": {}, 
+        "Ward Linh Tay": {}, 
+        "Ward Linh Dong": {}, 
+        "Ward Long Phuoc": {}, 
+        "Ward Long Binh": {}, 
+        "Ward Long Truong": {}, 
+        "Ward Long Thanh My": {}, 
+        "Ward Phu Huu": {}, 
+        "Ward Phuoc Binh": {}, 
+        "Ward Phuoc Long A": {}, 
+        "Ward Phuoc Long B": {}, 
+        "Ward Tam Phu": {}, 
+        "Ward Tam Binh": {}, 
+        "Ward Tang Nhon Phu A": {}, 
+        "Ward Tang Nhon Phu B": {}, 
+        "Ward Tan Phu": {}, 
+        "Ward Thanh My Loi": {}, 
+        "Ward Thao Dien": {}, 
+        "Ward Thu Thiem": {}, 
+        "Ward Truong Thanh": {}, 
+        "Ward Truong Tho": {}
+      },
+      "Tan Phu": {
+        "Ward Hoa Thanh": {}, 
+        "Ward Phu Thanh": {}, 
+        "Ward Hiep Tan": {}, 
+        "Ward Phu Trung": {}, 
+        "Ward Phu Tho Hoa": {}, 
+        "Ward Son Ky": {}, 
+        "Ward Tan Quy": {}, 
+        "Ward Tan Son Nhi": {}, 
+        "Ward Tan Thanh": {}, 
+        "Ward Tan Thoi Hoa": {}, 
+        "Ward Tay Thanh": {}
+      },
+      "Tan Binh": {
+        "Ward 1": {}, 
+        "Ward 2": {}, 
+        "Ward 3": {}, 
+        "Ward 4": {}, 
+        "Ward 5": {}, 
+        "Ward 6": {}, 
+        "Ward 7": {}, 
+        "Ward 8": {}, 
+        "Ward 9": {}, 
+        "Ward 10": {}, 
+        "Ward 11": {}, 
+        "Ward 12": {}, 
+        "Ward 13": {}, 
+        "Ward 14": {}, 
+        "Ward 15": {}
+      },
+      "Phu Nhuan": {
+        "Ward 1": {}, 
+        "Ward 2": {}, 
+        "Ward 3": {}, 
+        "Ward 4": {}, 
+        "Ward 5": {}, 
+        "Ward 7": {}, 
+        "Ward 8": {}, 
+        "Ward 9": {}, 
+        "Ward 10": {}, 
+        "Ward 11": {}, 
+        "Ward 13": {}, 
+        "Ward 15": {}, 
+        "Ward 17": {}
+      },
+      "Go Vap": {
+        "Ward 1": {}, 
+        "Ward 3": {}, 
+        "Ward 4": {}, 
+        "Ward 5": {}, 
+        "Ward 6": {}, 
+        "Ward 7": {}, 
+        "Ward 8": {}, 
+        "Ward 9": {}, 
+        "Ward 10": {}, 
+        "Ward 11": {}, 
+        "Ward 12": {}, 
+        "Ward 13": {}, 
+        "Ward 14": {}, 
+        "Ward 15": {}, 
+        "Ward 16": {}, 
+        "Ward 17": {}
+      },
+      "Binh Thanh": {
+        "Ward 1": {}, 
+        "Ward 2": {}, 
+        "Ward 3": {}, 
+        "Ward 5": {}, 
+        "Ward 6": {}, 
+        "Ward 7": {}, 
+        "Ward 11": {}, 
+        "Ward 12": {}, 
+        "Ward 13": {}, 
+        "Ward 14": {}, 
+        "Ward 15": {}, 
+        "Ward 17": {}, 
+        "Ward 19": {}, 
+        "Ward 21": {}, 
+        "Ward 22": {}, 
+        "Ward 24": {}, 
+        "Ward 25": {}, 
+        "Ward 26": {}, 
+        "Ward 27": {}, 
+        "Ward 28": {}
+      },
+      "Binh Chanh": {
+        "Town Tan Tuc": {}, 
+        "Commune An Phu Tay": {}, 
+        "Commune Binh Hung": {}, 
+        "Commune Binh Chanh": {}, 
+        "Commune Binh Loi": {}, 
+        "Commune Da Phuoc": {}, 
+        "Commune Hung Long": {}, 
+        "Commune Le Minh Xuan": {}, 
+        "Commune Pham Van Hai": {}, 
+        "Commune Phong Phu": {}, 
+        "Commune Quy Duc": {}, 
+        "Commune Tan Kien": {}, 
+        "Commune Tan Nhut": {}, 
+        "Commune Tan Quy Tay": {}, 
+        "Commune Vinh Loc A": {}, 
+        "Commune Vinh Loc B": {}
+      },
+      "Can Gio": {
+        "Town Can Thanh": {}, 
+        "Commune An Thoi Dong": {}, 
+        "Commune Binh Khanh": {}, 
+        "Commune Long Hoa": {}, 
+        "Commune Ly Nhon": {}, 
+        "Commune Tam Thon Hiep": {}, 
+        "Commune Thanh An": {}
+      },
+      "Cu Chi": {
+        "Town Cu Chi": {}, 
+        "Commune An Nhon Tay": {}, 
+        "Commune An Phu": {}, 
+        "Commune Binh My": {}, 
+        "Commune Hoa Phu": {}, 
+        "Commune Nhuan Duc": {}, 
+        "Commune Phu Hoa Dong": {}, 
+        "Commune Pham Van Co": {}, 
+        "Commune Phu My Hung": {}, 
+        "Commune Phuoc Thanh": {}, 
+        "Commune Phuoc Hiep": {}, 
+        "Commune Phuoc Vinh An": {}, 
+        "Commune Tan An Hoi": {}, 
+        "Commune Tan Phu Trung": {}, 
+        "Commune Tan Thong Hoi": {}, 
+        "Commune Tan Thanh Dong": {}, 
+        "Commune Tan Thanh Tay": {}, 
+        "Commune Thai My": {}, 
+        "Commune Trung An": {}, 
+        "Commune Trung Lap Thuong": {}, 
+        "Commune Trung Lap Ha": {}
+      },
+      "Hoc Mon": {
+        "Town Hoc Mon": {}, 
+        "Commune Ba Diem": {}, 
+        "Commune Nhi Binh": {}, 
+        "Commune Dong Thanh": {}, 
+        "Commune Tan Hiep": {}, 
+        "Commune Tan Xuan": {}, 
+        "Commune Tan Thoi Nhi": {}, 
+        "Commune Thoi Tam Thon": {}, 
+        "Commune Trung Chanh": {}, 
+        "Commune Xuan Thoi Thuong": {}, 
+        "Commune Xuan Thoi Dong": {}, 
+        "Commune Xuan Thoi Son": {}
+      },
+      "Nha Be": {
+        "Town Nha Be": {}, 
+        "Commune Hiep Phuoc": {}, 
+        "Commune Long Thoi": {}, 
+        "Commune Nhon Duc": {}, 
+        "Commune Phu Xuan": {}, 
+        "Commune Phuoc Loc": {}, 
+        "Commune Phuoc Kien": {}
+      }
+    },
+    "Da Nang": {
+      
     }
-  }
-  
+  },
+  America: {
+
+  },
+  Australia: {
+
+  },
+  Russia: {
+
+  },
+  China: {
+
+  },
+};
+
+window.onload = function() {
+  const selectCountry = document.getElementById("country"),
+        selectProvince = document.getElementById("province"),
+        selectDistrictCity = document.getElementById("district/city"),
+        selectWardTownCommune = document.getElementById("ward/town/commune"),
+        select = document.querySelectorAll("select")
+        
+        selectProvince.disabled = true,
+        selectDistrictCity.disabled = true,
+        selectWardTownCommune.disabled = true,
+
+        select.forEach(select => {
+          if(select.disabled == true) {
+            select.style.cursor = "auto"
+          }
+        })
+
+        for(let country in countryProvinceCityDistrictWard) {
+          selectCountry.options[selectCountry.options.length] = new Option(country, country)
+        }
+
+        // Country change
+        selectCountry.onchange = (e) => {
+          selectProvince.disabled = false
+          selectDistrictCity.disabled = true
+          selectWardTownCommune.disabled = true
+
+          selectProvince.length = 1
+          selectDistrictCity.length = 1
+          selectWardTownCommune.length = 1
+
+          for (let province in countryProvinceCityDistrictWard[e.target.value]) {
+            selectProvince.options[selectProvince.options.length] = new Option(province, province)
+          }
+        }
+
+        // Province change
+        selectProvince.onchange = (e) => {
+          selectDistrictCity.disabled = false
+          selectWardTownCommune.disabled = true
+
+          selectDistrictCity.length = 1
+          selectWardTownCommune.length = 1
+
+          for (let districtCity in countryProvinceCityDistrictWard[selectCountry.value][e.target.value]) {
+            selectDistrictCity.options[selectDistrictCity.options.length] = new Option(districtCity, districtCity)
+          }
+        }
+
+        // District/City change
+        selectDistrictCity.onchange = (e) => {
+          selectWardTownCommune.disabled = false
+
+          selectWardTownCommune.length = 1
+
+          let wardTownCommune = countryProvinceCityDistrictWard[selectCountry.value][selectDistrictCity.value][e.target.value]
+
+          for (let i = 0; i < wardTownCommune.length; ++i) {
+            selectWardTownCommune.options[selectWardTownCommune.options.length] = new Option(wardTownCommune[i], wardTownCommune[i])
+          }
+        }
+};
